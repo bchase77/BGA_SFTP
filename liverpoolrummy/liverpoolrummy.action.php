f27a8c055f4d39b9600362162b2363d4c6f4ac92
Bryan <bryan@shooftech.com>
2020-12-05 18:16:12 -0800

    Adding liverpoolrummy branch to git (oops).

---
+++ liverpoolrummy/liverpoolrummy.action.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ *------
+ * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
+ * LiverpoolRummy implementation : © Bryan Chase <bryanchase@yahoo.com>
+ *
+ * This code has been produced on the BGA studio platform for use on https://boardgamearena.com.
+ * See http://en.doc.boardgamearena.com/Studio for more information.
+ * -----
+ * 
+ * liverpoolrummy.action.php
+ *
+ * LiverpoolRummy main action entry point
+ *
+ *
+ * In this file, you are describing all the methods that can be called from your
+ * user interface logic (javascript).
+ *       
+ * If you define a method "myAction" here, then you can call it from your javascript code with:
+ * this.ajaxcall( "/liverpoolrummy/liverpoolrummy/myAction.html", ...)
+ *
+ */
+  
+  
+  class action_liverpoolrummy extends APP_GameAction
+  { 
+    // Constructor: please do not modify
+   	public function __default()
+  	{
+  	    if( self::isArg( 'notifwindow') )
+  	    {
+            $this->view = "common_notifwindow";
+  	        $this->viewArgs['table'] = self::getArg( "table", AT_posint, true );
+  	    }
+  	    else
+  	    {
+            $this->view = "liverpoolrummy_liverpoolrummy";
+            self::trace( "Complete reinitialization of board game" );
+      }
+  	} 
+  	
+    function parseNumberList($number_list_arg)
+    {
+        // Removing last ';' if exists
+        if (substr( $number_list_arg, -1 ) == ';' ) {
+            $number_list_arg = substr( $number_list_arg, 0, -1 );
+        }
+
+        if( $number_list_arg == '' ) {
+            return array();
+        } else {
+            return explode( ';', $number_list_arg );
+        }
+    }
+ 	// TODO: defines your action entry points there
+
+		public function playerHasReviewedHand()
+		{
+			self::trace("[bmc] ajaxcall for playerHasReviewedHand");
+            self::setAjaxMode();
+//			$player_id = self::getArg("player_id", AT_posint, true);
+            $this->game->playerHasReviewedHand();
+            self::ajaxResponse();
+		}
+        public function discardCard()
+        {
+			self::trace("[bmc] ajaxcall for discardCard");
+            self::setAjaxMode();
+			$player_id = self::getArg("player_id", AT_posint, true);
+            $card_id = self::getArg("id", AT_posint, true);
+            $this->game->discardCard( $card_id, $player_id );
+            self::ajaxResponse();
+        }
+		public function playerGoDown()
+		{
+			self::trace("[bmc] ajaxcall for playerGoDown");
+			self::setAjaxMode();
+			$cardIDGroupA = self::parseNumberList(self::getArg('cardGroupA', AT_numberlist, true));
+			$cardIDGroupB = self::parseNumberList(self::getArg('cardGroupB', AT_numberlist, true));
+			$cardIDGroupC = self::parseNumberList(self::getArg('cardGroupC', AT_numberlist, true));
+			$boardCardId = self::getArg('boardCardId', AT_alphanum, true);
+			$boardArea = self::getArg('boardArea', AT_alphanum, true);
+			$boardPlayer = self::getArg('boardPlayer', AT_alphanum, true);
+			$handItems = self::parseNumberList(self::getArg('handItems', AT_numberlist, true));
+			self::dump("[bmc] A:", $cardIDGroupA);
+			self::dump("[bmc] B:", $cardIDGroupB);
+			self::dump("[bmc] C:", $cardIDGroupC);
+			self::dump("[bmc] BC:", $boardCardId);
+			self::dump("[bmc] BA:", $boardArea);
+			self::dump("[bmc] BP:", $boardPlayer);
+			self::dump("[bmc] HI:", $handItems);
+
+			$this->game->playerGoDown( $cardIDGroupA, $cardIDGroupB, $cardIDGroupC, $boardCardId, $boardArea, $boardPlayer, $handItems);
+			self::ajaxResponse();
+		}
+        public function buyRequest()
+        {
+            self::setAjaxMode();
+			self::trace("[bmc] ajaxcall for buyRequest");
+			$player_id = self::getArg( "player_id", AT_posint, true );
+            $this->game->buyRequest( $player_id ); 
+            self::ajaxResponse();
+        }
+        // public function notBuyRequest()
+        // {
+            // self::setAjaxMode();
+			// self::trace("[bmc] ajaxcall for notBuyRequest");
+			// $player_id = self::getArg("player_id", AT_posint, true);
+            // $this->game->notBuyRequest( $player_id ); 
+            // self::ajaxResponse();
+        // }
+        public function drawCard()
+        {
+            self::setAjaxMode();
+			self::trace("[bmc] ajaxcall for drawCard");
+            $card_id = self::getArg("id", AT_posint, true);
+			$player_id = self::getArg("player_id", AT_posint, true);
+//			$drawSource = self::getArg("drawSource", AT_posint, true); // 0 == 'deck', 1 == 'discardPile'
+			$drawSource = self::getArg("drawSource", AT_alphanum, true);
+            $this->game->drawCard( $card_id, $drawSource, $player_id );
+            self::ajaxResponse();
+        }
+        public function playCard()
+        {
+            self::setAjaxMode();
+			self::trace("[bmc] ajaxcall for playCard");
+            $card_id = self::getArg("card_id", AT_posint, true);
+			$player_id = self::getArg("player_id", AT_posint, true);
+			$boardArea = self::getArg("boardArea", AT_alphanum, true);
+			$boardPlayer = self::getArg("boardPlayer", AT_alphanum, true);
+            $this->game->playCard($card_id, $player_id, $boardArea, $boardPlayer); 
+            self::ajaxResponse();
+        }
+        public function playCardMultiple()
+        {
+            self::setAjaxMode();
+			self::trace( "[bmc] ajaxcall for playCardMultiple" );
+			$card_ids = self::parseNumberList(self::getArg('card_ids', AT_numberlist, true));
+			self::dump("[bmc] playCardMultiple:", $card_ids);
+			$player_id = self::getArg( "player_id", AT_posint, true );
+			$boardArea = self::getArg( "boardArea", AT_alphanum, true );
+			$boardPlayer = self::getArg( "boardPlayer", AT_alphanum, true );
+            $this->game->playCardMultiple( $card_ids, $player_id, $boardArea, $boardPlayer ); 
+            self::ajaxResponse();
+        }
+    /*
+    
+    Example:
+  	
+    public function myAction()
+    {
+        self::setAjaxMode();     
+
+        // Retrieve arguments
+        // Note: these arguments correspond to what has been sent through the javascript "ajaxcall" method
+        $arg1 = self::getArg( "myArgument1", AT_posint, true );
+        $arg2 = self::getArg( "myArgument2", AT_posint, true );
+
+        // Then, call the appropriate method in your game logic, like "playCard" or "myAction"
+        $this->game->myAction( $arg1, $arg2 );
+
+        self::ajaxResponse( );
+    }
+    
+    */
+
+  }
---
+++ liverpoolrummy/material.inc.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ *------
+ * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
+ * LiverpoolRummy implementation : © Bryan Chase <bryanchase@yahoo.com>
+ * 
+ * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
+ * See http://en.boardgamearena.com/#!doc/Studio for more information.
+ * -----
+ *
+ * material.inc.php
+ *
+ * LiverpoolRummy game material description
+ *
+ * Here, you can describe the material of your game with PHP variables.
+ *   
+ * This file is loaded in your game logic class constructor, ie these variables
+ * are available everywhere in your game logic code.
+ *
+ */
+
+$this->colors = array(
+    1 => array( 'name' => clienttranslate('club'),
+                'nametr' => self::_('club') ),
+    2 => array( 'name' => clienttranslate('spade'),
+                'nametr' => self::_('spade') ),
+    3 => array( 'name' => clienttranslate('heart'),
+                'nametr' => self::_('heart') ),
+    4 => array( 'name' => clienttranslate('diamond'),
+                'nametr' => self::_('diamond') ),
+	5 => array( 'name' => clienttranslate('joker'),
+                'nametr' => self::_('wild') )
+);
+
+$this->values_label = array(
+    1 => clienttranslate('Ace'),
+    2 => '2',
+    3 => '3',
+    4 => '4',
+    5 => '5',
+    6 => '6',
+    7 => '7',
+    8 => '8',
+    9 => '9',
+    10 => '10',
+    11 => clienttranslate('Jack'),
+    12 => clienttranslate('Queen'),
+    13 => clienttranslate('King')
+);
+
+$this->handTypesFull = array( // Hand targets associate with the down areas:
+  0 => array( "Target" => "2 Sets; No more no less (Set is 3 any suit of same value; Run is 4 in sequence of same suit)",
+	"QtySets" => 2, "QtyRuns" => 0, "deal" => 10, "Area_A" => "Set", "Area_B" => "Set", "Area_C" => "Empty" ),
+  1 => array( "Target" => "1 Set and 1 Run; No more no less (Set is 3 any suit of same value; Run is 4 in sequence of same suit)",
+	"QtySets" => 1, "QtyRuns" => 1, "deal" => 11, "Area_A" => "Set", "Area_B" => "Run", "Area_C" => "Empty" ),
+  2 => array( "Target" => "2 Runs; No more no less (Set is 3 any suit of same value; Run is 4 in sequence of same suit)",
+	"QtySets" => 0, "QtyRuns" => 2, "deal" => 11, "Area_A" => "Run", "Area_B" => "Run", "Area_C" => "Empty" ),
+  3 => array( "Target" => "3 Sets; No more no less (Set is 3 any suit of same value; Run is 4 in sequence of same suit)",
+	"QtySets" => 3, "QtyRuns" => 0, "deal" => 12, "Area_A" => "Set", "Area_B" => "Set", "Area_C" => "Set" ),
+  4 => array( "Target" => "2 Sets and 1 Run; No more no less (Set is 3 any suit of same value; Run is 4 in sequence of same suit)",
+	"QtySets" => 2, "QtyRuns" => 1, "deal" => 12, "Area_A" => "Set", "Area_B" => "Set", "Area_C" => "Run" ),
+  5 => array( "Target" => "1 Set and 2 Runs; No more no less (Set is 3 any suit of same value; Run is 4 in sequence of same suit)",
+	"QtySets" => 1, "QtyRuns" => 2, "deal" => 12, "Area_A" => "Set", "Area_B" => "Run", "Area_C" => "Run" ),
+  6 => array( "Target" => "3 Runs; No more no less (Set is 3 any suit of same value; Run is 4 in sequence of same suit)",
+	"QtySets" => 0, "QtyRuns" => 3, "deal" => 12, "Area_A" => "Run", "Area_B" => "Run", "Area_C" => "Run" )
+);
+
+$this->setsRunsFull = array ( // Places in the downArea where the cards should go, per hand
+ 0 => array( "Area_A", "Area_B", "None",   "None",   "None",   "None" ),
+ 1 => array( "Area_A", "None",   "None",   "Area_B", "None",   "None" ),
+ 2 => array( "None",   "None",   "None",   "Area_A", "Area_B", "None" ),
+ 3 => array( "Area_A", "Area_B", "Area_C", "None",   "None",   "None" ),
+ 4 => array( "Area_A", "Area_B", "None",   "Area_C", "None",   "None" ),
+ 5 => array( "Area_A", "None",   "None",   "Area_B", "Area_C", "None" ),
+ 6 => array( "None",   "None",   "None",   "Area_A", "Area_B", "Area_C" )
+);
+/*
+$this->handTypesShort = array( // Hand targets associate with the down areas:
+  0 => array( "Target" => "2 Sets; No more no less (Set is 3 of same value; Run is 4 in sequence of same suit)",
+	"QtySets" => 2, "QtyRuns" => 0, "deal" => 10, "Area_A" => "Set", "Area_B" => "Set", "Area_C" => "Empty" ),
+  // 2 => array( "Target" => "2 Runs; No more no less (Set is 3 of same value; Run is 4 in sequence of same suit)",
+	// "QtySets" => 0, "QtyRuns" => 2, "deal" => 10, "Area_A" => "Run", "Area_B" => "Run", "Area_C" => "Empty" ),
+  // 4 => array( "Target" => "2 Sets and 1 Run; No more no less (Set is 3 of same value; Run is 4 in sequence of same suit)",
+	// "QtySets" => 2, "QtyRuns" => 1, "deal" => 12, "Area_A" => "Set", "Area_B" => "Set", "Area_C" => "Run" ),
+  // 6 => array( "Target" => "3 Runs; No more no less (Set is 3 of same value; Run is 4 in sequence of same suit)",
+	// "QtySets" => 0, "QtyRuns" => 3, "deal" => 12, "Area_A" => "Run", "Area_B" => "Run", "Area_C" => "Run" )
+);
+
+$this->setsRunsShort = array ( // Places in the downArea where the cards should go, per hand
+ 0 => array( "Area_A", "Area_B", "None",   "None",   "None",   "None" ),
+ // 2 => array( "None",   "None",   "None",   "Area_A", "Area_B", "None" ),
+ // 4 => array( "Area_A", "Area_B", "None",   "Area_C", "None",   "None" ),
+ // 6 => array( "None",   "None",   "None",   "Area_A", "Area_B", "Area_C" )
+);
+*/
+
+$this->handTypesShort = array( // Hand targets associate with the down areas:
+  0 => array( "Target" => "2 Sets; No more no less (Set is 3 of same value; Run is 4 in sequence of same suit)",
+	"QtySets" => 2, "QtyRuns" => 0, "deal" => 10, "Area_A" => "Set", "Area_B" => "Set", "Area_C" => "Empty" ),
+  2 => array( "Target" => "2 Runs; No more no less (Set is 3 of same value; Run is 4 in sequence of same suit)",
+	"QtySets" => 0, "QtyRuns" => 2, "deal" => 10, "Area_A" => "Run", "Area_B" => "Run", "Area_C" => "Empty" ),
+  4 => array( "Target" => "2 Sets and 1 Run; No more no less (Set is 3 of same value; Run is 4 in sequence of same suit)",
+	"QtySets" => 2, "QtyRuns" => 1, "deal" => 12, "Area_A" => "Set", "Area_B" => "Set", "Area_C" => "Run" ),
+  6 => array( "Target" => "3 Runs; No more no less (Set is 3 of same value; Run is 4 in sequence of same suit)",
+	"QtySets" => 0, "QtyRuns" => 3, "deal" => 12, "Area_A" => "Run", "Area_B" => "Run", "Area_C" => "Run" )
+);
+
+$this->setsRunsShort = array ( // Places in the downArea where the cards should go, per hand
+ 0 => array( "Area_A", "Area_B", "None",   "None",   "None",   "None" ),
+ 2 => array( "None",   "None",   "None",   "Area_A", "Area_B", "None" ),
+ 4 => array( "Area_A", "Area_B", "None",   "Area_C", "None",   "None" ),
+ 6 => array( "None",   "None",   "None",   "Area_A", "Area_B", "Area_C" )
+);
+
+$this->handTypesTwo = array( // Hand targets associate with the down areas:
+  0 => array( "Target" => "2 Sets; No more no less (Set is 3 of same value; Run is 4 in sequence of same suit)",
+	"QtySets" => 2, "QtyRuns" => 0, "deal" => 10, "Area_A" => "Set", "Area_B" => "Set", "Area_C" => "Empty" ),
+  1 => array( "Target" => "2 Runs; No more no less (Set is 3 of same value; Run is 4 in sequence of same suit)",
+	"QtySets" => 0, "QtyRuns" => 2, "deal" => 10, "Area_A" => "Run", "Area_B" => "Run", "Area_C" => "Empty" )
+);
+
+$this->setsRunsTwo = array ( // Places in the downArea where the cards should go, per hand
+ 0 => array( "Area_A", "Area_B", "None",   "None",   "None",   "None" ),
+ 1 => array( "None",   "None",   "None",   "Area_A", "Area_B", "None" )
+);
+
+$this->handTypesOne = array( // Hand targets associate with the down areas:
+  0 => array( "Target" => "2 Sets; No more no less (Set is 3 of same value; Run is 4 in sequence of same suit)",
+	"QtySets" => 2, "QtyRuns" => 0, "deal" => 10, "Area_A" => "Set", "Area_B" => "Set", "Area_C" => "Empty" )
+);
+
+$this->setsRunsOne = array ( // Places in the downArea where the cards should go, per hand
+ 0 => array( "Area_A", "Area_B", "None",   "None",   "None",   "None" )
+);
+
+/*
+  0 => array( "m2 Sets", 2, 0),
+  1 => array( "m1 Set and 1 Run", 1, 1),
+  2 => array( "m2 Runs", 0, 2),
+  3 => array( "m3 Sets", 3, 0),
+  4 => array( "m2 Sets and 1 Run", 2, 1),
+  5 => array( "m1 Set and 2 Runs", 1, 2),
+  6 => array( "m3 Runs", 0, 3)
+
+*/
+
+
+
+/*
+
+Example:
+
+$this->card_types = array(
+    1 => array( "card_name" => ...,
+                ...
+              )
+);
+
+*/
+
+
+/*
+$bob = array(2) {
+	[0]=> array(7) {
+		["Target"]=>string(83) "2 Sets; No more no less (Set is 3 of same value; Run is 4 in sequence of same suit)"
+		["QtySets"]=>int(2)
+		["QtyRuns"]=>int(0)
+		["deal"]=>int(10)
+		["Area_A"]=>string(3) "Set"
+		["Area_B"]=>string(3) "Set"
+		["Area_C"]=>string(5) "Empty"
+	}
+	[1]=>array(7) {
+		["Target"]=>string(83) "2 Runs; No more no less (Set is 3 of same value; Run is 4 in sequence of same suit)"
+		["QtySets"]=>int(0)
+		["QtyRuns"]=>int(2)
+		["deal"]=>int(10)
+		["Area_A"]=>string(3) "Run"
+		["Area_B"]=>string(3) "Run"
+		["Area_C"]=>string(5) "Empty"
+	}
+}
+*/
